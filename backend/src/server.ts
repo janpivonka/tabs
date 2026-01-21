5ffab3a8098fab595e19730b26ba5576f86a1c42
Jan Pivoňka <jan.pivonka.job@gmail.com>
2026-01-21 12:20:33 +0100

    parse server port from string into number because of railway needs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,7 +18,7 @@ app.get("/health", (_req, res) =>
 app.use(errorHandler);
 
 // Railway přiděluje port dynamicky, pokud není k dispozici, použije se 4000
-const PORT = process.env.PORT || 4000;
+const PORT = Number(process.env.PORT) || 4000;
 
 // Host '0.0.0.0' je nutný pro přístup z vnějšího světa v Docker/Cloud prostředí
 app.listen(PORT, '0.0.0.0', () => {
